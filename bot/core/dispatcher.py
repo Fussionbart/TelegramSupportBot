08e7200975f5457acfe83f01a37d787054da90d4
Fussionbart <146754297+Fussionbart@users.noreply.github.com>
2025-11-27 18:52:42 +0400

    Update dispatcher.py

--- bot/core/dispatcher.py
+++ bot/core/dispatcher.py
@@ -1,18 +1,8 @@
 from aiogram import Dispatcher
-
-# импортируем роутеры
 from bot.handlers.user import router as user_router
 from bot.handlers.admin import router as admin_router
 
-
 def register_all_handlers(dp: Dispatcher):
-    """
-    Здесь подключаем все хендлеры проекта.
-    """
-
-    # порядок важен:
-    # 1) админские — чтобы раньше ловили админа
-    # 2) пользовательские — чтобы ловили всех остальных
-
     dp.include_router(admin_router)
     dp.include_router(user_router)
+
