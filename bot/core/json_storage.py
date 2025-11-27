c5ad13407ca9ce247afc878a99e9d61ae725da3a
Fussionbart <146754297+Fussionbart@users.noreply.github.com>
2025-11-27 18:53:26 +0400

    Update json_storage.py

--- bot/core/json_storage.py
+++ bot/core/json_storage.py
@@ -1,42 +1,56 @@
 import json
 import os
-import threading
-
+import asyncio
 
 class JsonStorage:
 
     def __init__(self, path: str):
         self.path = path
-        self.lock = threading.Lock()
-
-        # создаём файл, если его нет
-        self._ensure_exists()
+        self.lock = asyncio.Lock()
 
     def _ensure_exists(self):
+        directory = os.path.dirname(self.path)
+        if directory and not os.path.exists(directory):
+            os.makedirs(directory, exist_ok=True)
+
         if not os.path.exists(self.path):
-            os.makedirs(os.path.dirname(self.path), exist_ok=True)
             with open(self.path, "w", encoding="utf-8") as f:
                 f.write("{}")
 
-    def load(self) -> dict:
-        with self.lock:
-            with open(self.path, "r", encoding="utf-8") as f:
-                try:
+    async def load(self) -> dict:
+        self._ensure_exists()
+        async with self.lock:
+            try:
+                with open(self.path, "r", encoding="utf-8") as f:
                     return json.load(f)
-                except json.JSONDecodeError:
-                    return {}
+            except (json.JSONDecodeError, FileNotFoundError):
+                return {}
 
-    def save(self, data: dict):
-        with self.lock:
+    async def save(self, data: dict):
+        self._ensure_exists()
+        async with self.lock:
+            temp_path = self.path + ".tmp"
+
+            with open(temp_path, "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=2, ensure_ascii=False)
+
+            os.replace(temp_path, self.path)
+
+
+    async def update(self, callback):
+        self._ensure_exists()
+        try:
+            with open(self.path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+        except:
+            data = {}
+
+        callback(data)
+
+        async with self.lock:
             temp_path = self.path + ".tmp"
             with open(temp_path, "w", encoding="utf-8") as f:
                 json.dump(data, f, indent=2, ensure_ascii=False)
 
-            # заменяем оригинальный файл
             os.replace(temp_path, self.path)
 
-    def update(self, callback):
-        with self.lock:
-            data = self.load()
-            callback(data)
-            self.save(data)
