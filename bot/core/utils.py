caf63a5cdfa05e8b3b37a4177820b5778d370d38
Fussionbart <146754297+Fussionbart@users.noreply.github.com>
2025-11-27 18:54:03 +0400

    Update utils.py

--- bot/core/utils.py
+++ bot/core/utils.py
@@ -1,35 +1,21 @@
-from aiogram import Bot
 from aiogram.exceptions import TelegramForbiddenError, TelegramBadRequest, TelegramRetryAfter
 import asyncio
 from config import ADMIN_ID
 from bot.core.loader import bot
 
-
-async def safe_send(chat_id: int, text: str):
-    """
-    Безопасная отправка сообщения:
-    - игнорирует ошибки, если юзер заблокировал бота
-    - корректно обрабатывает FloodWait
-    """
+async def safe_send(chat_id: int, text: str, reply_markup=None):
     try:
-        return await bot.send_message(chat_id, text)
+        return await bot.send_message(chat_id, text, reply_markup=reply_markup)
     except TelegramForbiddenError:
-        # пользователь заблокировал бота
         return None
     except TelegramBadRequest:
-        # сообщение нельзя отправить (например, слишком длинное)
         return None
     except TelegramRetryAfter as e:
-        # FloodWait — делаем паузу и пробуем снова
         await asyncio.sleep(e.retry_after)
-        return await safe_send(chat_id, text)
+        return await safe_send(chat_id, text, reply_markup)
     except Exception:
-        # любые другие ошибки — игнорируем
         return None
 
 
 def is_admin(user_id: int) -> bool:
-    """
-    Проверяет, является ли пользователь админом.
-    """
     return user_id == ADMIN_ID
