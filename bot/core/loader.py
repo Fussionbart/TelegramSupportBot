ad9c8dfeecf6f240876c0243a6cd5d37e392f250
Fussionbart <146754297+Fussionbart@users.noreply.github.com>
2025-11-27 18:53:02 +0400

    Update loader.py

--- bot/core/loader.py
+++ bot/core/loader.py
@@ -1,14 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
-
 from config import TOKEN
 
-
-# Инициализируем бота
 bot = Bot(
     token=TOKEN,
-    default=DefaultBotProperties(parse_mode="HTML")  # включим HTML разметку
+    default=DefaultBotProperties(parse_mode="HTML")
 )
 
-# Создаём диспетчер
 dp = Dispatcher()
